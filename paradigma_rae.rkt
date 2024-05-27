53e6835be4bc58eef9c6b0c1641a375dc5b2a7dd
jorgerebolledoa <aziel.az18@gmail.com>
2024-05-26 23:07:16 -0400

    cambios rkt orden definicion

--- paradigma_rae.rkt
+++ paradigma_rae.rkt
@@ -3,33 +3,34 @@
 (require json)
 (require net/url)
 
-
 (display "Ingresa una palabra: ")
 (define word (read-line))
 
-
 (define str1 "http://localhost:3000/api/")
-;(define str3 "martillo")
 (define result (string-append str1 word))
 
-
-;(define refino lambda(entrada))
-;  cond  
+(define (format-definiciones definicion)
+  (string-join (string-split definicion #px"\d+\. ") "\n\n"))
 
 (define (make-http-request url)
   (define response (get-pure-port url))
   (cond
     ((port? response)
-     (let ((body (port->string response)))
-       (display "Response body: ")
-       (newline)
-       (displayln body)))
-;       (refino url->string body)
+     (let* ((body (port->string response))
+            (json-data (string->jsexpr body)))
+       (cond
+         ((hash? json-data)
+          (let ((palabra (hash-ref json-data 'palabra))
+                (definicion (hash-ref json-data 'definicion)))
+            (displayln (string-append "Palabra: " palabra))
+            (displayln "Definiciones:")
+            (displayln (format-definiciones definicion))))
+         (else
+          (displayln "Error: Respuesta no es un JSON válido")))))
     (else
      (displayln "Error: Failed to make HTTP request"))))
 
-(make-http-request (string->url result)) 
-
+(make-http-request (string->url result))
 
 
 
